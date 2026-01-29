742e7bbe306f44a6296875adf04a37b4a008048d
ralphy127 <wybraniecrafal01@gmail.com>
2026-01-29 17:03:10 +0100

    Prepared to run the project on render.com

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -4,7 +4,8 @@ class Settings(BaseSettings):
     class Config:
         env_file = '.env'
         case_sensitive = False
-    
+        env_file_encoding = 'utf-8'
+        
     database_url: str = 'postgresql://postgres:postgres@localhost:5432/trip_manager'
     secret_key: str = 'dev-secret-key'
     frontend_url: str = 'http://localhost:5173'
